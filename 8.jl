8ec00d22e554d9e921c290225ac44eaf6fe53b19
SavelyPavlov <58021121+Savko-fokin@users.noreply.github.com>
2021-12-23 08:40:13 +0300

    Create 8.jl

---
+++ 8.jl
@@ -0,0 +1,20 @@
+using HorizonSideRobots
+include("roblib.jl")
+
+
+function main(r)
+    side=Ost
+    while isborder(r,Nord)==true # прохода сверху нет
+        putmarker!(r)
+        move_by_markers!(r,side)
+        side=inverse(side)
+    end
+end
+
+move_by_markers!(r,side) = while ismarker(r) move!(r,side) end
+
+inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2,4))
+
+
+r=Robot( animate=true)
+main1()
