c5e833b8406f27a871192220c470e1b965c747e7
SavelyPavlov <58021121+Savko-fokin@users.noreply.github.com>
2021-12-23 09:01:08 +0300

    Create 11.jl

---
+++ 11.jl
@@ -0,0 +1,7 @@
+function main(r::Robot)
+    for i in (North, East, South, West)
+        path = go_to_border_and_return_path!(r, i; go_around_barriers = true)
+        putmarker!(r)
+        go_by_path!(r, path)
+    end
+end
